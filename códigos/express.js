f358a848f3c877584c9c35ce897df082a7d07b5b
Chiquin <99442194+leoziN77@users.noreply.github.com>
2023-11-01 17:54:30 -0300

    api-projeto

--- códigos/express.js
+++ códigos/express.js
@@ -23,67 +23,66 @@ async function obterAgentesAleatorios() {
     }
 }
 
-
 app.get('/mapas', async (req, res) => { // http://localhost:3000/mapas
     try {
-      const respostaMapas = await axios.get(urlApiMapas);
-      const mapas = respostaMapas.data.data;
-  
-      const mapasFormatados = mapas.map(mapa => ({
-        label: mapa.displayName,
-        value: mapa.displayName
-      }));
-  
-      res.json(mapasFormatados);
-    } catch (error) {
-      res.status(500).json({ erro: 'Ocorreu um erro ao buscar mapas da API.' });
-    }
-  });
+        const respostaMapas = await axios.get(urlApiMapas);
+        const mapas = respostaMapas.data.data;
 
-app.get('/comps', async (req, res) => { 
+        const mapasFormatados = mapas.map(mapa => ({
+            label: mapa.displayName,
+            value: mapa.displayName
+        }));
+
+        res.json(mapasFormatados);
+    } catch (error) {
+        res.status(500).json({ erro: 'Ocorreu um erro ao buscar mapas da API.' });
+    }
+});
+
+app.get('/comps', async (req, res) => {
     try {
-        const modo = req.query.modo; 
-        const mapaEspecificado = req.query.mapa; 
+        const modo = req.query.modo;
+        const mapaEspecificado = req.query.mapa;
 
         if (!modo) {
-            res.status(400).json({ erro: 'Você deve especificar um modo na URL (todos ou especifico).' });
+            res.status(400).json({ erro: 'Você deve especificar um modo na URL (todos ou específico).' });
             return;
         }
 
         if (modo === 'todos') { // http://localhost:3000/comps?modo=todos
-            const mapsWithAgents = await getMapsAndAgents();
+            const mapasComAgentes = await obterMapasEAgentes();
 
-            if (mapsWithAgents) {
-                const result = mapsWithAgents.map(item => {
+            if (mapasComAgentes) {
+                const resultado = mapasComAgentes.map(item => {
                     const [mapa, agentes] = item.split(': ');
-                    const agenteLista = agentes.split(', ');
+                    const listaDeAgentes = agentes.split(', ');
 
                     return {
                         mapa,
-                        agentes: agenteLista
+                        agentes: listaDeAgentes
                     };
                 });
 
-                res.json(result);
+                res.json(resultado);
             } else {
                 res.status(500).json({ erro: 'Ocorreu um erro ao buscar dados da API de mapas.' });
             }
-        } else if (modo === 'especifico' && mapaEspecificado) { // http://localhost:3000/comps?modo=especifico&mapa=bind
+        } else if (modo === 'específico' && mapaEspecificado) { // http://localhost:3000/comps?modo=específico&mapa=bind
             const respostaMapas = await axios.get(urlApiMapas);
             const mapas = respostaMapas.data.data;
 
-            const mapaEspecificadoEmMaiusculas = mapaEspecificado.toUpperCase();
+            const mapaEspecificadoEmMaiúsculas = mapaEspecificado.toUpperCase();
 
-            const mapa = mapas.find(map => map.displayName.toUpperCase() === mapaEspecificadoEmMaiusculas);
+            const mapa = mapas.find(map => map.displayName.toUpperCase() === mapaEspecificadoEmMaiúsculas);
 
             if (!mapa) {
                 res.status(404).json({ erro: 'Mapa não encontrado.' });
                 return;
             }
 
-            const agentesAleatorios = await obterAgentesAleatorios();
+            const agentesAleatórios = await obterAgentesAleatorios();
             const map = `${mapa.displayName}`;
-            const agentes = agentesAleatorios.map(agente => `${agente}`);
+            const agentes = agentesAleatórios.map(agente => `${agente}`);
 
             res.json([
                 {
@@ -99,35 +98,34 @@ app.get('/comps', async (req, res) => {
     }
 });
 
-
 // Função para obter os mapas com agentes aleatórios
-async function getMapsAndAgents() {
+async function obterMapasEAgentes() {
     try {
-        const mapsResponse = await axios.get(urlApiMapas);
-        const maps = mapsResponse.data.data;
+        const respostaMapas = await axios.get(urlApiMapas);
+        const mapas = respostaMapas.data.data;
 
         // Mapas excluídos
-        const excludedMaps = ["Piazza", "District", "The Range", "Kasbah"];
+        const mapasExcluídos = ["Piazza", "District", "The Range", "Kasbah"];
 
-        const mapsWithAgents = await Promise.all(maps.map(async map => {
+        const mapasComAgentes = await Promise.all(mapas.map(async mapa => {
             try {
-                const mapDisplayName = map.displayName;
+                const nomeDoMapa = mapa.displayName;
 
-                if (excludedMaps.includes(mapDisplayName)) {
+                if (mapasExcluídos.includes(nomeDoMapa)) {
                     return null;
                 }
 
-                const randomAgents = await obterAgentesAleatorios();
+                const agentesAleatórios = await obterAgentesAleatorios();
 
-                return `${mapDisplayName}: ${randomAgents.join(', ')}`;
+                return `${nomeDoMapa}: ${agentesAleatórios.join(', ')}`;
             } catch (error) {
-                return `Erro ao buscar informações para o mapa ${mapDisplayName}`;
+                return `Erro ao buscar informações para o mapa ${nomeDoMapa}`;
             }
         }));
 
-        const filteredMaps = mapsWithAgents.filter(map => map !== null);
+        const mapasFiltrados = mapasComAgentes.filter(mapa => mapa !== null);
 
-        return filteredMaps;
+        return mapasFiltrados;
     } catch (error) {
         throw new Error('Erro ao buscar mapas da API.');
     }
